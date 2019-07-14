1848f033196fb9fc3fdbef49c18f3e9595226afe
Wendy Liga <wendy.devs@gmail.com>
2019-07-14 22:50:30 +0700

    initial commit

---
+++ ar-milky-way/ViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ViewController.swift
+//  ar-milky-way
+//
+//  Created by Wendy Liga on 14/07/19.
+//  Copyright © 2019 Wendy Liga. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+        sceneView.debugOptions = [SCNDebugOptions.showWorldOrigin]
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+        
+        drawSun()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    private func drawSun(){
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.05))
+        sun.position = SCNVector3(0, 0, 0)
+        sun.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "sun")
+        
+        sceneView.scene.rootNode.addChildNode(sun)
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
